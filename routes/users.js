b965d09e80d49d48bef9126a97e2653e3af907ea
Kevin <kevin.mavier@hotmail.com>
2023-12-09 00:03:46 +0100

    list of todo

--- routes/users.js
+++ routes/users.js
@@ -1,16 +1,31 @@
+//#region Imports
+
 var express = require('express');
 var router = express.Router();
-
-
 require('../models/connection');
+
 const User = require('../models/users');
+
 const { checkBody } = require('../modules/checkBody');
 
+//#endregion
 
 
 
-router.get('/', function(req, res) {
-  
-});
+// TODO : Create user in database
+
+
+// TODO : Get All users
+
+
+// TODO : Set user as admin
+
+
+// TODO : If Admin => can delete one user
+
+
+// TODO : Find password by Email
+
+
 
 module.exports = router;
