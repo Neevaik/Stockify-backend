45b4bae6ca6c033e5e417e378e9b9152043259ce
samuel <samuelzentou@gmail.com>
2023-12-11 09:38:49 +0100

    mise en place set up lundi

--- routes/users.js
+++ routes/users.js
@@ -6,11 +6,29 @@ var router = express.Router();
 const User = require('../models/users');
 const { checkBody } = require('../modules/checkBody');
 
+// creation d 'un token unique par utilisateur;
+const uid2 = require('uid2');
+
+//hashage du mot de passe ;
+const bcrypt = require('bcrypt');
 
 
 
 router.post('/signup', (req, res)=> {
-  
+  if (!checkBody(req.body, ['storeName', 'username', 'password','email'])) {
+    res.json({ result: false, error: 'Missing or empty fields' });
+    return;
+  }
+    User.findOne({username :{$regex : new RegExp(req.body.username,'i')}}).then(data =>{
+      if (data === null){
+        const hash= bcrypt.hashSync(req.body.password,10);
+      }
+      
+    })
+
+
+
+
 });
 
 router.post('/signin', (req, res)=> {
