82cd88737dda9240e64eeed0d7c49f7d6f0342e2
duvfpro <duvfpro@outlook.com>
2023-12-13 11:48:41 +0100

    remise en état du back et model product

--- models/products.js
+++ models/products.js
@@ -2,13 +2,13 @@ const mongoose = require('mongoose');
 
 const productSchema = mongoose.Schema({
     name: String,
-    image:String,
-    stock:Number,
-    soldAt:[{date:Date,quantity:Number}],
-    restockAt: [{date:Date,quantity:Number}],
+    image: String,
+    stock: Number,
+    soldAt: [{date: Date, quantity: Number}],
+    restockAt: [{date: Date, quantity: Number}],
     category: [{type: mongoose.Schema.Types.ObjectId, ref : 'categories'}], 
-  });
-  
-  const Product = mongoose.model('products', productSchema);
-  
-  module.exports = Product;
+});
+
+const Product = mongoose.model('products', productSchema);
+
+module.exports = Product;
